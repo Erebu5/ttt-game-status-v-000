d9d76d85092ef011fbb4a523c8fb82deb1f99713
Jessica Mott <wigmore3.14@gmail.com>
2017-09-20 23:11:49 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,19 +3,22 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-#W inning Combo
+# Winning Combos
 WIN_COMBINATIONS = [
-  [0, 1, 2], # top row
-  [3, 4, 5], # middle row
-  [6, 7, 8], # bottom row
-  [0, 3, 6], # left column
-  [1, 4, 7], # middle column
-  [2, 5, 8], # right column
-  [0, 4, 8], # diagonal top-bottom
-  [2, 4, 6]  # diagonal bottom-top
+  [ 0, 1, 2 ], # top row
+  [ 3, 4, 5 ], # middle row
+  [ 6, 7, 8 ], # bottom row
+  [ 0, 3, 6 ], # left column
+  [ 1, 4, 7 ], # middle column
+  [ 2, 5, 8 ], # right column
+  [ 0, 4, 8 ], # diagonal top-bottom
+  [ 2, 4, 6 ]  # diagonal bottom-top
 ]
 
-#Won?
+# Won?
 def won?(board)
   WIN_COMBINATIONS.each do |win_combo|
+    if win_combo.all? { |token| board[token] == "X" }
+      return win_combo
+    elsif win_combo.all? { |token| board[token] == "O" }
 end
