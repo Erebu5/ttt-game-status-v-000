89172a5b14d1291c81d051e5899052ca93e8a4c0
Jessica Mott <wigmore3.14@gmail.com>
2017-09-20 23:08:09 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,14 +3,19 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Winning Combinations
+#W inning Combo
 WIN_COMBINATIONS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
+  [0, 1, 2], # top row
+  [3, 4, 5], # middle row
+  [6, 7, 8], # bottom row
+  [0, 3, 6], # left column
+  [1, 4, 7], # middle column
+  [2, 5, 8], # right column
+  [0, 4, 8], # diagonal top-bottom
+  [2, 4, 6]  # diagonal bottom-top
 ]
+
+#Won?
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combo|
+end
